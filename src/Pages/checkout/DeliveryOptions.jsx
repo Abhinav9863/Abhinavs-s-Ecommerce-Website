054e10ac6c228cda61a3bd2eca811c490af129d6
Abhinav vishnu mohan <abhinavvishnumohan21@gmail.com>
2025-10-02 23:00:34 +0530

    Crate update delivery option feature

--- src/Pages/checkout/DeliveryOptions.jsx
+++ src/Pages/checkout/DeliveryOptions.jsx
@@ -26,6 +26,7 @@ export function DeliveryOptions({deliveryOptions, cartItem ,  loadCart}) {
             <input
               type="radio"
               checked={deliveryOption.id === cartItem.deliveryOptionId}
+              onChange={() => {}}
               className="delivery-option-input"
               name={`delivery-option-1-${cartItem.product}`}
             />
